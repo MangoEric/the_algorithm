19546b205f250a0479f8dc8e5508484a76a4e75b
MangoEric <fgh1937@yahoo.com>
2024-03-11 23:41:35 +0900

    py - 01 - print

---
+++ 0_py_practice/01_print.py
@@ -0,0 +1,15 @@
+# print
+print('hello world')
+
+print('hello', 'world')
+
+# print with end
+print('hello', 'world', end='wow')
+
+print()
+
+# print number
+print(1,2,3,4,5)
+
+# print with separator
+print(1,2,3,4,5, sep=',')
