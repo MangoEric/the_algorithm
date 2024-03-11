d8e0510e59a2731ceef7ba319d799ae93a2526b4
MangoEric <fgh1937@yahoo.com>
2024-03-11 23:55:20 +0900

    py - 03 - str

---
+++ 0_py_practice/03_str.py
@@ -0,0 +1,31 @@
+'''
+String
+'''
+
+print('Seize your Moment!')
+print("Seize your Moment!")
+print("'Hello'") # 'Hello'
+print('"Hello"') # "Hello"
+
+# Escape Character
+print('"\'good"')
+print('hello \nworld')
+print('''\
+hello
+world\
+''')
+
+
+'''
+String Operations
+'''
+
+print('hello' + ' world')
+print('hello ' * 3)
+
+# int to str
+print('hello my age is ' + str(20) + ' years old')
+# str to int
+print(int('3'))
+# str length
+print(len('abcde'))
