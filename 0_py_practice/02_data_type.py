5df88278abc7b38ca27fe67986d01151d7573681
MangoEric <fgh1937@yahoo.com>
2024-03-11 23:55:10 +0900

    py - 02 - data type

---
+++ 0_py_practice/02_data_type.py
@@ -0,0 +1,16 @@
+'''
+Basic Data Types
+'''
+
+# 1. Integer
+print(type(1))
+
+# 2. Float
+print(type(1.5))
+
+# 3. Boolean
+print(type(True))
+print(type(False))
+
+# 4. String
+print(type('Hello World!'))
