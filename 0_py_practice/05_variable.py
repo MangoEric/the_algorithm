8ff599e8ca90bb4b642d3dbc10e6619364408d85
MangoEric <fgh1937@yahoo.com>
2024-03-12 23:59:14 +0900

    py - 05 - variable

---
+++ 0_py_practice/05_variable.py
@@ -0,0 +1,34 @@
+
+a = 1
+b = 2
+
+print(a + b)
+print(a - b)
+
+a = 10
+print(a)
+
+a = a + 1
+print(a)
+
+a += 1
+print(a)
+
+a -= 1
+print(a)
+
+a *= 2
+print(a)
+
+a //= 2
+print(a)
+
+a %= 2
+print(a)
+
+'''
+문자 복합 연산자
+'''
+c = 'Wow!'
+c *= 3
+print(c) # Wow!Wow!Wow!
